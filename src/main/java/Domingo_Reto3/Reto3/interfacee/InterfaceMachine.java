b78f8ac8c755a8fe979ae16559469abdb97bd04f
Alejandra G <maria.gonzalez1.mt@correo.usa.edu.co>
2021-10-18 17:52:23 -0500

    first commit

---
+++ src/main/java/Domingo_Reto3/Reto3/interfacee/InterfaceMachine.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package Domingo_Reto3.Reto3.interfacee;
+
+
+import Domingo_Reto3.Reto3.model.Machine;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ *
+ * @author USUARIO
+ */
+public interface InterfaceMachine extends CrudRepository<Machine,Integer> {
+    
+}
---
+++ src/main/java/Domingo_Reto3/Reto3/repository/RepositorioMachine.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Domingo_Reto3.Reto3.repository;
+
+import Domingo_Reto3.Reto3.interfacee.InterfaceMachine;
+import Domingo_Reto3.Reto3.model.Machine;
+
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author USUARIO
+ */
+@Repository
+public class RepositorioMachine {
+
+    @Autowired
+    private InterfaceMachine crud;
+
+    public List<Machine> getAll() {
+        return (List<Machine>) crud.findAll();
+    }
+
+    public Optional<Machine> getMachine(int id) {
+        return crud.findById(id);
+    }
+
+    public Machine save(Machine machine) {
+        return crud.save(machine);
+    }
+
+    public void delete(Machine machine) {
+        crud.delete(machine);
+    }
+
+}
